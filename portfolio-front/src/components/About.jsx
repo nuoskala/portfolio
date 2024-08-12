b0705ca7e856595ef15c9d3b154002faadba426b
Clara Nuoskala <clara.nuoskala@protonmail.com>
2024-08-12 10:26:10 +0300

    style: add skeleton for skills display

--- portfolio-front/src/components/About.jsx
+++ portfolio-front/src/components/About.jsx
@@ -1,5 +1,28 @@
 const About = () => (
-    <h1>The about page</h1>
+
+    <div>    
+      <h1>The about page</h1>
+
+      <div className="container">
+
+        <div className="item">
+          <p>front end</p>
+        </div>
+
+        <div className="item">
+          <p>back end</p>
+        </div>
+
+        <div className="item">
+          <p>other programming tools</p>
+
+        </div>
+
+
+      </div>
+
+    </div>
+
   )
 
 export default About
