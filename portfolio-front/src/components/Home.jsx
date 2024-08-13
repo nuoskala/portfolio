310a83ccbff9d7dd773997067c6040f03f497c67
Clara Nuoskala <clara.nuoskala@protonmail.com>
2024-08-13 20:30:25 +0300

    feat/style: add intro section

--- portfolio-front/src/components/Home.jsx
+++ portfolio-front/src/components/Home.jsx
@@ -1,36 +1,36 @@
 import About from './About'
 import Projects from './Projects'
 
+import pic from '../../img/portrait.png'
 
-const Home = () => (
+const Intro = () => {
+
+  return (
+    <div className='intro'>
+
+    <div className='intro-pic'> <img src={pic} alt="a picture of me"></img> </div>
+
+    <div className='intro-text'>
+      <p className='intro-text-welcome'>Welcome!</p>
+      <p>I am currently learning full stack web development.</p>
+      <p>This is where I display the <a className='intro-link' href="#projects"> projects</a> I have done and those I am currently working on.</p>
+      <p>You can also learn a bit <a className='intro-link' href="#projects"> about me</a> or have a look at my <a className='intro-link' href="#projects"> resume</a>.</p>
+    </div>
+
+    </div>
+
+  )
+}
+
+const Home = () => {
+
+  
+  return (
     <div>
-    <h1>The home page</h1>
-    <div>
-        <p>Lorem ipsum</p>
-        <p>Lorem ipsum</p>
-        <p>Lorem ipsum</p>
-        <p>Lorem ipsum</p>
-        <p>Lorem ipsum</p>
-        <p>Lorem ipsum</p>
-        <p>Lorem ipsum</p>
-        <p>Lorem ipsum</p>
-        <p>Lorem ipsum</p>
-        <p>Lorem ipsum</p>
-        <p>Lorem ipsum</p>
-        <p>Lorem ipsum</p>
-        <p>Lorem ipsum</p>
-        <p>Lorem ipsum</p>
-        <p>Lorem ipsum</p>
-        <p>Lorem ipsum</p>
-        <p>Lorem ipsum</p>
-        <p>Lorem ipsum</p>
-        <p>Lorem ipsum</p>
-        <p>Lorem ipsum</p>
-        <p>Lorem ipsum</p>
-        <p>Lorem ipsum</p>
-        <p>Lorem ipsum</p>
-        <p>Lorem ipsum</p>
-        <p>Lorem ipsum</p>
+
+    <div id="home">
+    <Intro />
+    </div>
 
     <div id="about" >
     <About/>
@@ -40,8 +40,9 @@ const Home = () => (
     <Projects/>
     </div>
 
-    </div>
+
     </div>
   )
+}
 
 export default Home
