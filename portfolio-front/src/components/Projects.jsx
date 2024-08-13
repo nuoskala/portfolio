a6bc37d65a5d01bad7917207d9167c58fd83f249
Clara Nuoskala <clara.nuoskala@protonmail.com>
2024-08-14 01:46:08 +0300

    feat/style: add project section

--- portfolio-front/src/components/Projects.jsx
+++ portfolio-front/src/components/Projects.jsx
@@ -1,5 +1,90 @@
-const Projects = () => (
-  <h1>Projects</h1>
+import { FaGithub } from "react-icons/fa";
+import { RxOpenInNewWindow } from "react-icons/rx";
+import parse from "html-react-parser";
+
+const projectsData = [
+  {
+      title: 'Portfolio',
+      description: 'This website. It includes a presentation of myself, and of the projects I made. <br> I am still working on it to include more features and improve the look and responsiveness.',
+      tools: [
+          'ReactJS',
+          'HTML/CSS'
+      ],
+      githubLink: 'https://github.com/nuoskala/portfolio',
+      pageLink: '',
+      ongoing: true,
+      id: 'portfolio'
+  },
+]
+
+const openInNewTab = (url) => {
+  window.open(url, '_blank', 'noopener, noreferrer');
+}
+
+const ButtonGithub = ({githubLink}) => {
+  if (githubLink != '') {
+    return (       
+      <button className='project-link-button' 
+      onClick={() => openInNewTab(githubLink)}> <FaGithub className='project-link-icon'/>
+      </button>
+   )
+  }
+}
+
+const ButtonPagelink = ({pageLink}) => {
+  if (pageLink != '') {
+    return (       
+      <button className='project-link-button' 
+      onClick={() => openInNewTab(pageLink)}> <RxOpenInNewWindow className='project-link-icon'/>
+      </button>
+   )
+  }
+}
+
+const Ongoing = ({ongoing}) => {
+  if (ongoing) {
+    return (       
+      <div className="project-status-current">Ongoing</div>
+   )
+  }
+}
+
+const ProjectItem = ({title, description, tools, githubLink, pageLink, ongoing}) => (
+  <div className="project">
+    <div className="project-preview"></div>
+    <div className="project-info">
+    <div className="project-title">{title}</div>
+      <div className="project-description">{parse(description)}</div>
+      <div className="project-tool-container">{tools.map((p, index) => <div className="project-tool" key={index}>{p}</div>)}</div>
+      <div className="project-links">
+      <ButtonGithub githubLink={githubLink}/>
+      <ButtonPagelink pageLink={pageLink}/>
+      </div>
+    </div>
+    <Ongoing ongoing={ongoing}/>
+  </div>
 )
+
+
+const Projects = () => {
+  return (
+  <div>
+  <h1>Projects</h1>
+
+  <div className="project-container">
+    {projectsData.map(p =>
+      <ProjectItem
+          key={p.id}
+          title={p.title}
+          description={p.description}
+          tools={p.tools}
+          githubLink={p.githubLink}
+          pageLink={p.pageLink}
+          ongoing={p.ongoing}
+      />)}
+  </div>
+  </div>
+)
+}
 
 export default Projects
